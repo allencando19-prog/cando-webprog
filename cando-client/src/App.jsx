3eac10338e1abdef1a8b9234fb6defefdb6805ab
allencando19-prog <allencando19@gmail.com>
2026-03-27 17:32:49 +0800

    lab-act2

--- cando-client/src/App.jsx
+++ cando-client/src/App.jsx
@@ -1,26 +1,38 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
-import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// HomePage Structure
+import Layout from "./components/Layout";
+import ArticlePage from "./pages/ArticlePage";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+
+const routes = [
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <HomePage />,
+      },
+      {
+        path: "about",
+        element: <AboutPage />,
+      },
+      {
+        path: "articles",
+        element: <ArticlePage />,
+      },
+    ],
+  },
+];
+
+const router = createBrowserRouter(routes);
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div className="App">
-        <header className="App-header">
-          <h1>Welcome to My React App!</h1>
-          <p>
-            Name: [Will Allen Cando]
-            <br />
-            Email: [allencando19@gmail.com]
-            <br />
-            Other Personal Info:
-          </p>
-        </header>
-      </div>
+      <RouterProvider router={router} />
     </>
   );
 }
